f1e29fd18f39391784c68f63b8b25592f0b24160
junha <goodjunha@gmail.com>
2023-07-04 16:14:20 +0900

    Feat:: 경로 변경

--- Projects/Mint-iOS/Sources/ModuleFactory/ModuleFactory.swift
+++ Projects/Mint-iOS/Sources/ModuleFactory/ModuleFactory.swift
@@ -8,10 +8,30 @@ public class ModuleFactory {
 
 extension ModuleFactory: ModuleFactoryInterface {
     
+    public func findPasswordVC() -> Presentation.FindPassswordViewController {
+        let findPassswordVC = FindPassswordViewController()
+        
+        return findPassswordVC
+    }
+    
+    
+    public func findIdVC() -> Presentation.FindIdViewController {
+        let findIdVC = FindIdViewController()
+        
+        return findIdVC
+    }
+    
+    
     public func loginVC() -> Presentation.LoginViewController {
         let loginVC = LoginViewController()
 
         return loginVC
+    }
+    
+    public func signupVC() -> Presentation.BaseFindVC {
+        let signupVC = BaseFindVC()
+        
+        return signupVC
     }
     
     public func mainVC() -> Presentation.MainViewController {
