db2d5fe454d7745189acde64ff5176af232f5a12
junha <goodjunha@gmail.com>
2023-07-04 17:29:39 +0900

    Refactor:: commint

--- Projects/Presentation/Sources/ViewController/SignUp/SignUpViewController.swift
+++ Projects/Presentation/Sources/ViewController/SignUp/SignUpViewController.swift
@@ -1,11 +1,3 @@
-//
-//  SignUpViewController.swift
-//  Presentation
-//
-//  Created by 박준하 on 2023/07/04.
-//  Copyright © 2023 Mint-iOS. All rights reserved.
-//
-
 import UIKit
 import SnapKit
 import Then
